537c0a58fd9beee0ae87c0669892be29c1ce037b
Aureus, Carlo <carlo_aureus@intuit.com>
2022-09-22 15:47:45 -0700

    adding unit test for FieldCoordinatesFormattingUtil

---
+++ src/test/java/com/intuit/graphql/authorization/util/FieldCoordinatesFormattingUtilTest.java
@@ -0,0 +1,33 @@
+package com.intuit.graphql.authorization.util;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import graphql.schema.FieldCoordinates;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Test;
+
+public class FieldCoordinatesFormattingUtilTest {
+
+  @Test
+  public void toStringTestEmpty() {
+    String actual = FieldCoordinatesFormattingUtil.toString(Collections.emptySet());
+    assertThat(actual).isEqualTo("");
+  }
+
+  @Test
+  public void toStringTestNullInput() {
+    String actual = FieldCoordinatesFormattingUtil.toString(null);
+    assertThat(actual).isEqualTo("");
+  }
+
+  @Test
+  public void toStringTestOneFieldCoordinate() {
+    Set<FieldCoordinates> fieldCoordinatesSet = new HashSet<>();
+    fieldCoordinatesSet.add(FieldCoordinates.coordinates("TestParentType", "testFieldName"));
+    String actual = FieldCoordinatesFormattingUtil.toString(fieldCoordinatesSet);
+    assertThat(actual).isEqualTo("TestParentType.testFieldName");
+  }
+
+}
