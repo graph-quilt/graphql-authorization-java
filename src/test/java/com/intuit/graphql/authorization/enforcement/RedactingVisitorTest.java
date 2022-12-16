58545262ddd16f72c63a1217fedc84aa4147d2e6
Carlo Feliciano Aureus <carlo_aureus@intuit.com>
2022-12-15 21:32:44 -0800

    edge test cases for RedactingVisitor

--- src/test/java/com/intuit/graphql/authorization/enforcement/RedactingVisitorTest.java
+++ src/test/java/com/intuit/graphql/authorization/enforcement/RedactingVisitorTest.java
@@ -3,6 +3,7 @@ package com.intuit.graphql.authorization.enforcement;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -14,6 +15,8 @@ import graphql.GraphqlErrorException;
 import graphql.analysis.QueryVisitorFieldEnvironment;
 import graphql.execution.ExecutionContext;
 import graphql.language.Field;
+import graphql.language.Node;
+import graphql.language.SourceLocation;
 import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLSchema;
@@ -46,23 +49,26 @@ public class RedactingVisitorTest {
   @Mock
   private GraphQLSchema graphQLSchema;
   @Mock
-  private TraverserContext traverserContext;
+  private TraverserContext<Node> traverserContext;
   @Mock
   private NodeZipper nodeZipper;
 
-  Queue<NodeZipper> zippers = new ArrayDeque<>();
+  private Queue<NodeZipper> zippers = new ArrayDeque<>();
 
   @Mock
   private QueryVisitorFieldEnvironment queryVisitorFieldEnvironment;
   @Mock
   private GraphQLFieldDefinition fieldDefinition;
 
-  private static final GraphQLObjectType PARENT_TYPE = GraphQLObjectType.newObject().name("ParentType").build();
-  private static final Field FIELD = Field.newField("foo").build();
+  private static final GraphQLObjectType PARENT_TYPE = GraphQLObjectType.newObject()
+      .name("ParentType").build();
+  private static final Field FIELD = Field.newField("foo")
+      .sourceLocation(SourceLocation.EMPTY)
+      .build();
 
   private RedactingVisitor subjectUnderTest;
 
-  private List<GraphQLError> graphQLErrorList = new ArrayList<>();
+  GraphqlErrorException authExtensionGraphqlError = GraphqlErrorException.newErrorException().build();
 
   @Before
   public void setup() {
@@ -71,7 +77,9 @@ public class RedactingVisitorTest {
     when(traverserContext.getVar(NodeZipper.class)).thenReturn(nodeZipper);
     when(traverserContext.getSharedContextData()).thenReturn(zippers);
 
-    when(instrumentationState.getTypeFieldPermissionVerifier()).thenReturn(typeFieldPermissionVerifier);
+    when(instrumentationState.getTypeFieldPermissionVerifier()).thenReturn(
+        typeFieldPermissionVerifier);
+    List<GraphQLError> graphQLErrorList = new ArrayList<>();
     when(instrumentationState.getAuthzErrors()).thenReturn(graphQLErrorList);
 
     subjectUnderTest = new RedactingVisitor(instrumentationState, executionContext, authzListener,
@@ -86,13 +94,37 @@ public class RedactingVisitorTest {
   }
 
   @Test
-  public void visitField_typeFieldPermitted_authExtensionNotAllowed() {
+  public void visitField_accessToTypeFieldPermittedAcceptedByAuthExtension_NoGraphQLErrors() {
     // GIVEN
-    GraphqlErrorException graphqlErrorException = GraphqlErrorException.newErrorException().build();
-
+    when(instrumentationState.isEnforce()).thenReturn(true);
     when(typeFieldPermissionVerifier.isPermitted(eq(PARENT_TYPE), eq(fieldDefinition)))
         .thenReturn(true);
-    when(authorizationExtension.authorize(any())).thenReturn(FieldAuthorizationResult.createDeniedResult(graphqlErrorException));
+
+    when(authorizationExtension.authorize(any())).thenReturn(
+        FieldAuthorizationResult.ALLOWED_FIELD_AUTH_RESULT);
+
+    // WHEN
+    subjectUnderTest.visitField(queryVisitorFieldEnvironment);
+
+    // THEN
+    verify(queryVisitorFieldEnvironment, times(2)).getFieldDefinition();
+    verify(typeFieldPermissionVerifier, times(1)).isPermitted(any(), any());
+    verify(authzListener, never()).onFieldRedaction(any(), any());
+    verify(nodeZipper, never()).deleteNode();
+    verify(authorizationExtension, times(1)).authorize(any());
+
+    assertThat(instrumentationState.getAuthzErrors()).hasSize(0);
+  }
+
+  @Test
+  public void visitField_accessToTypeFieldPermittedButDeniedByAuthExtension_providesCorrectGraphQLErrors() {
+    // GIVEN
+    when(instrumentationState.isEnforce()).thenReturn(true);
+    when(typeFieldPermissionVerifier.isPermitted(eq(PARENT_TYPE), eq(fieldDefinition)))
+        .thenReturn(true);
+
+    when(authorizationExtension.authorize(any())).thenReturn(
+        FieldAuthorizationResult.createDeniedResult(authExtensionGraphqlError));
 
     // WHEN
     subjectUnderTest.visitField(queryVisitorFieldEnvironment);
@@ -102,9 +134,73 @@ public class RedactingVisitorTest {
     verify(typeFieldPermissionVerifier, times(1)).isPermitted(any(), any());
     verify(authzListener, times(1)).onFieldRedaction(any(), any());
     verify(nodeZipper, times(1)).deleteNode();
+    verify(authorizationExtension, times(1)).authorize(any());
 
-    assertThat(graphQLErrorList).hasSize(1);
-    assertThat(graphQLErrorList.get(0)).isSameAs(graphqlErrorException);
+    assertThat(instrumentationState.getAuthzErrors()).hasSize(1);
+    assertThat(instrumentationState.getAuthzErrors().get(0)).isSameAs(authExtensionGraphqlError);
+  }
+
+  @Test
+  public void visitField_accessToTypeFieldIsNotPermitted_authExtensionIsBypassedAndProvidesCorrectGraphQLErrors() {
+    // GIVEN
+    when(instrumentationState.isEnforce()).thenReturn(true);
+    when(typeFieldPermissionVerifier.isPermitted(eq(PARENT_TYPE), eq(fieldDefinition)))
+        .thenReturn(false);
+
+    // WHEN
+    subjectUnderTest.visitField(queryVisitorFieldEnvironment);
+
+    // THEN
+    verify(queryVisitorFieldEnvironment, times(1)).getFieldDefinition();
+    verify(typeFieldPermissionVerifier, times(1)).isPermitted(any(), any());
+    verify(authzListener, times(1)).onFieldRedaction(any(), any());
+    verify(nodeZipper, times(1)).deleteNode();
+    verify(authorizationExtension, never()).authorize(any());
+
+    assertThat(instrumentationState.getAuthzErrors()).hasSize(1);
+    assertThat(instrumentationState.getAuthzErrors().get(0).getMessage()).contains(
+        "403 - Not authorized to access field");
+  }
+
+  @Test
+  public void visitField_authzNotEnforcedDeniedByAuthExtension_providesCorrectGraphQLErrors() {
+    // GIVEN
+    when(instrumentationState.isEnforce()).thenReturn(false);
+
+    when(authorizationExtension.authorize(any())).thenReturn(
+        FieldAuthorizationResult.createDeniedResult(authExtensionGraphqlError));
+
+    // WHEN
+    subjectUnderTest.visitField(queryVisitorFieldEnvironment);
+
+    // THEN
+    verify(queryVisitorFieldEnvironment, times(2)).getFieldDefinition();
+    verify(typeFieldPermissionVerifier, never()).isPermitted(any(), any());
+    verify(authzListener, times(1)).onFieldRedaction(any(), any());
+    verify(nodeZipper, times(1)).deleteNode();
+
+    assertThat(instrumentationState.getAuthzErrors()).hasSize(1);
+    assertThat(instrumentationState.getAuthzErrors().get(0)).isSameAs(authExtensionGraphqlError);
+  }
+
+  @Test
+  public void visitField_authzNotEnforcedAllowedByAuthExtension_noGraphQLErrors() {
+    // GIVEN
+    when(instrumentationState.isEnforce()).thenReturn(false);
+    when(authorizationExtension.authorize(any())).thenReturn(
+        FieldAuthorizationResult.ALLOWED_FIELD_AUTH_RESULT);
+
+    // WHEN
+    subjectUnderTest.visitField(queryVisitorFieldEnvironment);
+
+    // THEN
+    verify(queryVisitorFieldEnvironment, times(2)).getFieldDefinition();
+    verify(typeFieldPermissionVerifier, never()).isPermitted(any(), any());
+    verify(authzListener, never()).onFieldRedaction(any(), any());
+    verify(nodeZipper, never()).deleteNode();
+    verify(authorizationExtension, times(1)).authorize(any());
+
+    assertThat(instrumentationState.getAuthzErrors()).hasSize(0);
   }
 
 }
